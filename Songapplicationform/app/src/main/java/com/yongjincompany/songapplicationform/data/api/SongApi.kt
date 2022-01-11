9a7903c0fe2234a18f177c97c86487e77c7e1e15
Leesin0222 <dragon02229@naver.com>
2022-01-12 03:51:37 +0900

    📑 ::  ApiInterface 😍

--- Songapplicationform/app/src/main/java/com/yongjincompany/songapplicationform/data/api/SongApi.kt
+++ Songapplicationform/app/src/main/java/com/yongjincompany/songapplicationform/data/api/SongApi.kt
@@ -1,4 +1,24 @@
 package com.yongjincompany.songapplicationform.data.api
 
+import android.widget.EditText
+import com.yongjincompany.songapplicationform.data.RecieveSongResponseDTO
+import com.yongjincompany.songapplicationform.data.SendSongRequestDTO
+import retrofit2.Call
+import retrofit2.Response
+import retrofit2.http.*
+
 interface SongApi {
+
+    @POST("/song")
+    fun postSong(
+        @Body sendSong: String
+    ): Call<SendSongRequestDTO>
+
+    @GET("/song")
+    fun getSongList() : Call<List<RecieveSongResponseDTO>>
+
+    @DELETE("/song/{id}")
+    suspend fun deleteSong(
+        @Path ("id") id: Int
+    )
 }
