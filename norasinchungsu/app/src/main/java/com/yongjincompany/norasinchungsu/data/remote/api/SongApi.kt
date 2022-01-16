7d56d9718ac4eb81e86b44feb7ba90f50bbc7dbe
Leesin0222 <dragon02229@naver.com>
2022-01-16 16:56:58 +0900

    📑 :: @GET 메서드 추가

--- norasinchungsu/app/src/main/java/com/yongjincompany/norasinchungsu/data/remote/api/SongApi.kt
+++ norasinchungsu/app/src/main/java/com/yongjincompany/norasinchungsu/data/remote/api/SongApi.kt
@@ -1,4 +1,9 @@
 package com.yongjincompany.norasinchungsu.data.remote.api
 
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface SongApi {
+    @GET("/song")
+    suspend fun getTodos(): Response<List<SongApi>>
 }
